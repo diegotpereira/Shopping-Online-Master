50fd22ad3136de3a03e664205b53c950f77570e5
Diego <diegoteixeirapereira@hotmail.com>
2021-09-13 15:15:32 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Online</title>
+</head>
+<body>
+    <!-- O CONTAINER PRINCIPAL COMEÇA AQUI -->
+    <div class="main">
+        <!-- O HEADER COMEÇA AQUI -->
+        <div class="header">
+            <a href="#"><img id="logo_image" src="#"></a>
+            <img id="logo_image" src="#" style="width: 60%;" />
+        </div>
+        <!-- O HEADER TERMINA AQUI -->
+
+        <!-- BARRA DE NAVEGAÇÃO COMEÇA AQUI -->
+        <div class="menuBar">
+            <ul id="menu">
+                <li><a href="">Home</a></li>
+                <li><a href="">Produtos</a></li>
+                <li><a href="">Conta</a></li>
+                <li><a href="">Entrar</a></li>
+                <li><a href="">Carrinho de Compras</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+
+            <div id="form">
+                <form method="get" action="" enctype="multipart/form-data">
+                   <input type="text" name="" placeholder="" />
+                   <input type="submit" name="buscar" value="buscar" />
+                </form>
+            </div>
+        </div>
+
+        <!-- BARRA DE NAVEGAÇÃO TERMINA AQUI -->
+
+        <!-- A SESSÃO CONTENT COMEÇA AQUI -->
+        <div class="content">
+            <div id="sidebar">
+                <div id="sidebar_title">Categorias</div>
+                <ul id="gatos">
+
+                </ul>
+
+                <div id="sidebar_title">Marcas</div>
+                <ul id="gatos">
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
